7b10fb33f5e76d59e3d33303be855cd9d5375110
Franck Royer <franck@status.im>
2022-02-01 11:28:28 +1100

    Get data for all packages

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
 const registry = require('npm-stats')(["registry.npmjs.org"])
-const { Parser } = require('json2csv');
+const {Parser} = require('json2csv');
 const fs = require("fs");
 
 let promises = []
 
-let date = new Date("2021-04-26")
+let date = new Date("2021-05-04")
 const now = new Date();
 
-const data = [];
+const data = {};
 
 while (date <= now) {
-    const until = new Date();
-    until.setDate(date.getDate() + 6);
+    const until = new Date(date);
+    until.setDate(until.getDate() + 6);
     const since = new Date(date);
+    const dateStr = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(since);
+
+    data[dateStr] = {};
+
     promises.push(new Promise((resolve, reject) => {
         registry.module("js-waku").totalDownloads({since, until}, function (err, downloads) {
             if (err) reject(err);
-
-            const dateStr = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(since);
-            data.push({date: dateStr, "js-waku": downloads})
+            data[dateStr].jsWaku = downloads;
             resolve();
         })
-
     }))
+    promises.push(new Promise((resolve, reject) => {
+        registry.module("hashconnect").totalDownloads({since, until}, function (err, downloads) {
+            if (err) reject(err);
+            data[dateStr].hashconnect = downloads;
+            resolve();
+        })
+    }))
+    promises.push(new Promise((resolve, reject) => {
+        registry.module("@walletconnect/core").totalDownloads({since, until}, function (err, downloads) {
+            if (err) reject(err);
+            data[dateStr].walletConnect = downloads;
+            resolve();
+        })
+    }))
+
     date.setDate(date.getDate() + 7);
 }
 
 Promise.all(promises).then(() => {
+    const array = []
+
+    for (let key in data) {
+        array.push({
+            date: key,
+            'js-waku': data[key].jsWaku,
+            'wallet connect': data[key].walletConnect,
+            'hashconnect': data[key].hashconnect
+        })
+    }
+
     const parser = new Parser();
-    const csv = parser.parse(data);
+    const csv = parser.parse(array);
 
     // write to a new file named 2pac.txt
     fs.writeFile('downloads.csv', csv, (err) => {
